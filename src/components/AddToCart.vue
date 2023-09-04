232eb5c43d7bc328fdd867d4008d1936ff9553d8
Celinaki <celina1999@live.se>
2023-09-04 20:43:02 +0200

    add and increase functions working

---
+++ src/components/AddToCart.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+import { onMounted, ref, } from 'vue'
+import {useIncrementStore } from '../stores/increment'
+
+
+const store = useIncrementStore()
+onMounted(() => {
+    
+})
+
+// defineProps<{
+//   message: string,
+//   bobaType: object
+// }>()
+const props = defineProps<{
+  message: string,
+  bobaType: {
+    id: number,
+        title?: string,
+        price?: number,
+        bobaImg?:string,
+        amount: number,
+  }
+}>()
+const boba = props.bobaType;
+function decreaseAmount(){
+    if(boba.amount <= 0){
+        return
+    }else{
+      boba.amount--
+    console.log(boba.amount)      
+    }
+
+
+}
+function increaseAmount(){
+    boba.amount++
+    console.log(boba.amount)
+}
+
+</script>
+
+<template >
+    <div>
+        <h2 v-on:click="increaseAmount">add</h2>
+  
+        <h2 v-on:click="decreaseAmount">dec</h2>
+        <h1>{{ message }}</h1>
+    </div>
+    
+
+</template>
+
+<style scoped>
+div{
+    background-color: #DAABFF;
+    position: sticky;
+    bottom: 0;
+    width: 100%;
+    height: 68px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 17px 17px 0px 0px;
+}
+h1{
+    color: #8F00FF;
+    font-size: 22px;
+    font-style: italic;
+    font-weight: 900;
+}
+h2{
+    padding: 1rem;
+}
+</style>
--- src/views/SingleBobaView.vue
+++ src/views/SingleBobaView.vue
@@ -1,17 +1,11 @@
 
-<template>
-    <div v-if="chosenBoba">
-    
-        <h1>{{ chosenBoba.title }}</h1>
-<img :src=chosenBoba.teaImg alt="">
-<h3>{{ chosenBoba.price }}</h3>
-    </div>
-</template>
+
 
 <script setup lang="ts">
 import { onMounted, ref, } from 'vue'
 import {useGetAllBobasStore } from '../stores/getallbobas'
 import { useRoute } from 'vue-router'
+import AddToCart from '@/components/AddToCart.vue';
 const route = useRoute()
 const store = useGetAllBobasStore()
 
@@ -20,11 +14,67 @@ onMounted(() => {
     const bobaId = Number(route.params.bubbleTeaId)
     const bobaArray = store.teaArray;
     const chosenBoba = bobaArray.find((bobaTea) => bobaTea.id === bobaId)
+    const thisBoba = {
+        id: bobaId,
+        title: chosenBoba?.title,
+        price: chosenBoba?.price,
+        bobaImg:chosenBoba?.teaImg,
+        amount: 0,
+    }
 </script>
 
+<template>
+    <div v-if="chosenBoba" class="page-container">
+        <article class="img-holder">
+            <span class="image-background"></span>
+         <img :src=chosenBoba.teaImg :alt="`Image of the drink ${chosenBoba.title}`">
+        </article>
+        <h1>{{ chosenBoba.title }}</h1>
+        <h2>{{ chosenBoba.price }}</h2>
+        
+    </div>
+    <AddToCart message="Add" :bobaType="thisBoba" />
+</template>
 
-
-<style scoped>
+<style scoped lang="scss">
+.page-container{
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    padding: 49px 1.5rem;
+ .img-holder{
+    background-color: rgb(220, 183, 250);
+    border-radius: 50%;
+    width: 250px;
+    height: 250px;
+    display: flex;
+    justify-content: center;
+    z-index: 1;
+    position: relative;
+    margin-top: 2rem;
+        .image-background{
+        width: 230px;
+        height: 220px;
+        background-color: rgb(202, 152, 243);
+        z-index: -10;
+        border-radius: 50%;
+        transform: translateY(0px);
+        position: absolute;
+        bottom: 0;
+        }
+        img{
+            height: 100%;
+            transform: translateY(-25%) rotate(-10deg);
+            position: absolute;
+        }
+    }  
+    h1{
+        font-size: 28px;
+        align-self: flex-start;
+        margin: 2.5rem 0rem;
+    } 
+}
 
 
 </style>
