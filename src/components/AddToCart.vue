c23cb2abb37aeafa360cab903998346e908cf1c9
Celinaki <celina1999@live.se>
2023-09-06 13:53:13 +0200

    changing text to icons + -, textshadow

--- src/components/AddToCart.vue
+++ src/components/AddToCart.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { onMounted, ref, } from 'vue'
-import {useIncrementStore } from '../stores/increment'
+import { useIncrementStore } from '../stores/increment'
 
 
 const store = useIncrementStore()
@@ -13,30 +13,30 @@ onMounted(() => {
 //   bobaType: object
 // }>()
 const props = defineProps<{
-  message: string,
-  bobaType: {
-    id: number,
+    message: string,
+    bobaType: {
+        id: number,
         title?: string,
         price?: number,
-        bobaImg?:string,
+        bobaImg?: string,
         amount: number,
-  }
+    }
 }>()
 const boba = props.bobaType;
 let bobaAmount = ref<number>(boba.amount)
 
-function decreaseAmount(){
-    if(boba.amount <= 0){
+function decreaseAmount() {
+    if (boba.amount <= 0) {
         return
-    }else{
-      boba.amount--
-      bobaAmount.value--
-    console.log(boba.amount)      
+    } else {
+        boba.amount--
+        bobaAmount.value--
+        console.log(boba.amount)
     }
 
 
 }
-function increaseAmount(){
+function increaseAmount() {
     boba.amount++
     bobaAmount.value++
 
@@ -47,17 +47,18 @@ function increaseAmount(){
 
 <template >
     <div>
-        <h1 v-on:click="increaseAmount">add</h1>
-  <h1>{{ bobaAmount }}</h1>
-        <h1 v-on:click="decreaseAmount">dec</h1>
-  
-    </div>
-    
+        <h1 v-on:click="decreaseAmount">-</h1>
 
+        <h1>{{ bobaAmount }}</h1>
+        <h1 v-on:click="increaseAmount">+</h1>
+
+
+
+    </div>
 </template>
 
 <style scoped>
-div{
+div {
     background-color: #DAABFF;
     position: sticky;
     bottom: 0;
@@ -68,12 +69,14 @@ div{
     align-items: center;
     border-radius: 17px 17px 0px 0px;
 }
-h1{
-    color: #8F00FF;
+
+h1 {
+    color: #B454FF;
     font-size: 22px;
     font-style: italic;
     font-weight: 900;
     padding: 1rem;
-}
+    text-shadow: 3px 2px 0px white;
 
+}
 </style>
