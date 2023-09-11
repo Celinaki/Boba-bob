a4602a3d543ed7df490b4de18d0d93f12dcf88f0
Celinaki <celina1999@live.se>
2023-09-11 21:44:13 +0200

    pushing object into array

--- src/stores/addToCart.ts
+++ src/stores/addToCart.ts
@@ -3,7 +3,9 @@ import { defineStore } from 'pinia'
 
 export const useCartStore = defineStore('cart', () => {
   const items = ref([])
-
+  const addToCart = (boba) => {
+    items.value.push(boba)
+  }
 
   return { items }
 })
