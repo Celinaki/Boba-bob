8d361ac5bc048898695ad4de3ff6885fe27cdc99
Celinaki <celina1999@live.se>
2023-09-11 21:35:47 +0200

    base for cart store setup

---
+++ src/stores/addToCart.ts
@@ -0,0 +1,9 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useCartStore = defineStore('cart', () => {
+  const items = ref([])
+
+
+  return { items }
+})
