e352ede1e324c75759ca6a8e40d4074f9b84e448
Celinaki <celina1999@live.se>
2023-09-12 23:52:45 +0200

    base of addtocart function inside addtocart component

--- src/components/AddToCart.vue
+++ src/components/AddToCart.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
 import { onMounted, ref, computed } from 'vue'
 import { useCartStore } from '../stores/addToCart'
-
-
 const store = useCartStore()
+
 onMounted(() => {
     boba.amount
 
@@ -16,13 +15,13 @@ onMounted(() => {
 const props = defineProps<{
     message: string,
     bobaType: {
-    id: number,
+        id: number,
         title?: string,
         price?: number,
         bobaImg?: string,
         amount: number,
-        addExtraBoba:boolean,
-        addExtraJelly:boolean,
+        addExtraBoba: boolean,
+        addExtraJelly: boolean,
     }
 }>()
 const boba = props.bobaType;
@@ -35,15 +34,15 @@ let addJelly = ref<boolean>(boba.addExtraJelly)
 
 const totalPrice = computed(() => {
     let price = bobaPrice.value * bobaAmount.value;
-    if(addBoba.value === true){
-        price +=5* bobaAmount.value;
+    if (addBoba.value === true) {
+        price += 5 * bobaAmount.value;
     }
-    if(addJelly.value === true){
-        price +=5* bobaAmount.value;
+    if (addJelly.value === true) {
+        price += 5 * bobaAmount.value;
     }
     return price
-    }
- )
+}
+)
 
 function decreaseAmount() {
     if (boba.amount <= 1) {
@@ -61,7 +60,21 @@ function increaseAmount() {
 }
 
 let added = ref(false)
+function addBobaToCart(bobaItem: {
+    id: number,
+    title?: string,
+    price?: number,
+    bobaImg?: string,
+    amount: number,
+    addExtraBoba: boolean,
+    addExtraJelly: boolean
+}) {
+    bobaItem.id = boba.id;
+    bobaItem.title = boba.title;
+    
+    store.addToCart(bobaItem)
 
+}
 
 </script>
 
@@ -73,14 +86,9 @@ let added = ref(false)
             <h1 v-on:click="increaseAmount">+</h1>
         </section>
 
-        <button 
-        v-if="!added"
-        @click="(()=> added = true)">Add to cart {{ totalPrice }} SEK</button>
-        <button 
-       
-        :class="added ? 'expanded' : 'animated-button'"
-        :style="{  transition:'850ms' }"
-        @click="(()=> added = true)">Proceed to checkout {{ totalPrice }} SEK</button>
+        <button v-if="!added" @click="(() => added = true)">Add to cart {{ totalPrice }} SEK</button>
+        <button :class="added ? 'expanded' : 'animated-button'" :style="{ transition: '850ms' }"
+            @click="(() => added = true)">Proceed to checkout {{ totalPrice }} SEK</button>
     </div>
 </template>
 
@@ -95,7 +103,8 @@ div {
     justify-content: space-between;
     align-items: center;
     border-radius: 17px 17px 0px 0px;
-    button{
+
+    button {
         border: none;
         border-radius: 8px;
         margin: 1rem;
@@ -104,40 +113,40 @@ div {
         font-size: 16px;
         font-weight: bold;
     }
-    .animated-button{
+
+    .animated-button {
         width: 200px;
         position: absolute;
-        top:.2rem;
+        top: .2rem;
         bottom: 1rem;
         left: 1rem;
         transition: display 850ms ease;
         pointer-events: none;
         overflow: hidden;
         visibility: hidden;
-        
+
     }
-    .expanded{        
+
+    .expanded {
         pointer-events: all;
         transition: display 850ms ease;
         display: flex;
         width: 100%;
     }
+
     section {
-    display: flex;
+        display: flex;
+
         h1 {
-        color: #742daa;
-        font-size: 24px;
-        /* font-style: italic; */
-        font-weight: 900;
-        padding: 1rem;
-        text-shadow: 3px 2px 0px white;
-    
+            color: #742daa;
+            font-size: 24px;
+            /* font-style: italic; */
+            font-weight: 900;
+            padding: 1rem;
+            text-shadow: 3px 2px 0px white;
+
 
         }
     }
 }
-
-
-
-
 </style>
