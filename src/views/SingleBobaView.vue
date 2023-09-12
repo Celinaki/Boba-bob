9ab220ad5eb2bc777493526c265a16d2f6f44297
Celinaki <celina1999@live.se>
2023-09-12 23:53:11 +0200

    code structure

--- src/views/SingleBobaView.vue
+++ src/views/SingleBobaView.vue
@@ -3,7 +3,7 @@
 
 <script setup lang="ts">
 import { onMounted, ref, } from 'vue'
-import {useGetAllBobasStore } from '../stores/getallbobas'
+import { useGetAllBobasStore } from '../stores/getallbobas'
 import { useRoute } from 'vue-router'
 import AddToCart from '@/components/AddToCart.vue';
 const route = useRoute()
@@ -12,20 +12,20 @@ const store = useGetAllBobasStore()
 onMounted(() => {
 
 })
-    const bobaId = Number(route.params.bubbleTeaId)
-    const bobaArray = store.teaArray;
-    const extraBoba = ref(false)
-    const addJelly = ref(false)
-    const chosenBoba = bobaArray.find((bobaTea) => bobaTea.id === bobaId)
-    const thisBoba = {
-        id: bobaId,
-        title: chosenBoba?.title,
-        price: chosenBoba?.price,
-        bobaImg:chosenBoba?.teaImg,
-        amount: 1,
-        addExtraBoba: extraBoba,
-        addExtraJelly: addJelly
-    }
+const bobaId = Number(route.params.bubbleTeaId)
+const bobaArray = store.teaArray;
+const extraBoba = ref(false)
+const addJelly = ref(false)
+const chosenBoba = bobaArray.find((bobaTea) => bobaTea.id === bobaId)
+const thisBoba = {
+    id: bobaId,
+    title: chosenBoba?.title,
+    price: chosenBoba?.price,
+    bobaImg: chosenBoba?.teaImg,
+    amount: 1,
+    addExtraBoba: extraBoba,
+    addExtraJelly: addJelly
+}
 
 </script>
 
@@ -33,79 +33,83 @@ onMounted(() => {
     <div v-if="chosenBoba" class="page-container">
         <article class="img-holder">
             <span class="image-background"></span>
-         <img :src=chosenBoba.teaImg :alt="`Image of the drink ${chosenBoba.title}`">
+            <img :src=chosenBoba.teaImg :alt="`Image of the drink ${chosenBoba.title}`">
         </article>
         <h1>{{ chosenBoba.title }}</h1>
         <h2>{{ chosenBoba.price }} SEK</h2>
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, iste aperiam vitae officiis iusto excepturi sint pariatur velit eos placeat.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, iste aperiam vitae officiis iusto excepturi
+            sint pariatur velit eos placeat.</p>
         <section>
-            <p>Extra boba +5 SEK 
-                <input type="checkbox" 
-                v-model="extraBoba" 
-                onchange="(()=>{extraBoba = !extraBoba})" 
-                name="" id=""> </p>
-            <p>Lägg till yelly +5 SEK 
-                <input type="checkbox"
-                 v-model="addJelly"
-                 onchange="(()=>{addJelly = !addJelly})"  
-                 name=""  id=""></p>
+            <p>Extra boba +5 SEK
+                <input type="checkbox" v-model="extraBoba" onchange="(()=>{extraBoba = !extraBoba})" name="" id="">
+            </p>
+            <p>Lägg till yelly +5 SEK
+                <input type="checkbox" v-model="addJelly" onchange="(()=>{addJelly = !addJelly})" name="" id="">
+            </p>
         </section>
     </div>
     <AddToCart message="Add" :bobaType="thisBoba" />
 </template>
 
 <style scoped lang="scss">
-.page-container{
+.page-container {
     display: flex;
     justify-content: center;
     flex-direction: column;
     align-items: center;
     padding: 49px 1.5rem;
- .img-holder{
-    background-color: rgb(220, 183, 250);
-    border-radius: 50%;
-    width: 250px;
-    height: 250px;
-    display: flex;
-    justify-content: center;
-    z-index: 1;
-    position: relative;
-    margin-top: 2rem;
-        .image-background{
-        width: 230px;
-        height: 220px;
-        background-color: rgb(202, 152, 243);
-        z-index: -10;
+
+    .img-holder {
+        background-color: rgb(220, 183, 250);
         border-radius: 50%;
-        transform: translateY(0px);
-        position: absolute;
-        bottom: 0;
+        width: 250px;
+        height: 250px;
+        display: flex;
+        justify-content: center;
+        z-index: 1;
+        position: relative;
+        margin-top: 2rem;
+
+        .image-background {
+            width: 230px;
+            height: 220px;
+            background-color: rgb(202, 152, 243);
+            z-index: -10;
+            border-radius: 50%;
+            transform: translateY(0px);
+            position: absolute;
+            bottom: 0;
         }
-        img{
+
+        img {
             height: 100%;
             transform: translateY(-20%) rotate(-13deg);
             position: absolute;
         }
-    }  
-    h1,h2{
+    }
+
+    h1,
+    h2 {
         font-size: 28px;
         align-self: flex-start;
         margin: 2.5rem 0rem 1rem 0rem;
         color: #292929;
         font-weight: 900;
         font-style: italic;
-    } 
-    h2{
+    }
+
+    h2 {
         font-size: 22px;
         margin: 0rem 0rem 1rem 0rem;
     }
-    section{
+
+    section {
         width: 100%;
-        p{
-            
-        }
-        input[type="checkbox"]{
+
+        p {}
+
+        input[type="checkbox"] {
             height: 1.2rem;
             width: 1.2rem;
             accent-color: #a331b9;
@@ -114,6 +118,4 @@ onMounted(() => {
     }
 
 }
-
-
 </style>
