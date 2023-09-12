d960bfc57d596702a3e6223bf764de619c34a12f
Celinaki <celina1999@live.se>
2023-09-12 23:49:22 +0200

    returning addtocart function,defining interface

--- src/stores/addToCart.ts
+++ src/stores/addToCart.ts
@@ -2,10 +2,25 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useCartStore = defineStore('cart', () => {
-  const items = ref([])
-  const addToCart = (boba) => {
+  const items = ref<Array<{ id: number;
+    title?: string;
+    price?: number;
+    bobaImg?: string;
+    amount: number;
+    addExtraBoba: boolean;
+    addExtraJelly: boolean;}>>([])
+
+  const addToCart = (boba:{
+    id: number;
+    title?: string;
+    price?: number;
+    bobaImg?: string;
+    amount: number;
+    addExtraBoba: boolean;
+    addExtraJelly: boolean;
+  }) => {
     items.value.push(boba)
   }
 
-  return { items }
+  return { items, addToCart }
 })
